6378117a59f653739e82904798eecea767806fd0
Stoyan Ivanov <stoyan.ivanov@dreamix.eu>
2020-04-19 22:33:19 +0300

    fix login.component.ts

--- src/main/webapp/app/login/login.component.ts
+++ src/main/webapp/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer, ViewChild } from '@angular/core';
+import { LoginService } from 'app/core/login/login.service';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'jhi-login',
@@ -6,7 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  @ViewChild('username', { static: false })
+  username?: ElementRef;
+
+  authenticationError = false;
+
+  loginForm = this.fb.group({
+    username: [''],
+    password: [''],
+    rememberMe: [false]
+  });
+
+  constructor(private loginService: LoginService, private renderer: Renderer, private router: Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  login(): void {
+    this.loginService
+      .login({
+        username: this.loginForm.get('username')!.value,
+        password: this.loginForm.get('password')!.value,
+        rememberMe: this.loginForm.get('rememberMe')!.value
+      })
+      .subscribe(
+        () => {
+          this.authenticationError = false;
+          if (
+            this.router.url === '/account/register' ||
+            this.router.url.startsWith('/account/activate') ||
+            this.router.url.startsWith('/account/reset/')
+          ) {
+            this.router.navigate(['']);
+          }
+        },
+        () => (this.authenticationError = true)
+      );
+  }
+
+  cancel(): void {
+    this.authenticationError = false;
+    this.loginForm.patchValue({
+      username: '',
+      password: ''
+    });
+  }
 }
